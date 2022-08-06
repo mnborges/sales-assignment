416eeb8bedc0b07987987dd4cb8dedf93da54824
Maieza N. Borges <maieza.borges@gmail.com>
2022-08-06 09:52:41 -0400

    finished edit page

--- pages/manage/[sid].js
+++ pages/manage/[sid].js
@@ -1,27 +1,146 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthUserContext";
-import { getSaleById, getSalesIds } from "../../lib/sanityData";
-/*
-Page thats display complete information (especially commission related) about a sale
-sid = sales id
-*/
-const Sale = ({ sale }) => {
+import calculateCommission from "../../lib/commission";
+import { getSaleById, getSalesIds, updateSale } from "../../lib/sanityData";
+
+const Edit = ({ sale }) => {
   const router = useRouter();
   const { loading, authUser } = useAuth();
+  const [clientName, setClientName] = useState(sale.client);
+  const [productName, setProducName] = useState(sale.product);
+  const [price, setPrice] = useState(sale.price);
+  const [date, setDate] = useState(sale.date);
 
   useEffect(() => {
-    //user not registered
+    // user not registered
     if (!loading && !authUser) {
       router.push("/login");
     }
   }, [loading, authUser, router]);
-  return <p>Sale: {sale._id}</p>;
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  // prevent who's not sale's seller to edit it
+  if (!loading && authUser.uid !== sale.seller.authUserId) {
+    return <h1 className="text-4xl p-6 text-center">Not allowed.</h1>;
+  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      confirm(
+        "Please confirm you'd like to submit these changes.\nOnce you do, the sale will be pending approval."
+      )
+    ) {
+      // recalculate commission and update sanity content lake
+      const commission = await calculateCommission(price);
+      const doc = {
+        client: clientName,
+        seller: {
+          _ref: sale.seller._id,
+          _type: "reference",
+        },
+        product: productName,
+        price: parseFloat(price),
+        date: date,
+        commission: commission,
+        status: "pending",
+      };
+      return await updateSale(sale._id, doc)
+        .then(() => {
+          alert("Sale successfully updated.");
+          router.push("/sales");
+        })
+        .catch((error) => {
+          throw new Error(error.message);
+        });
+    }
+    return console.log("canceled");
+  };
+  const dateMax = new Date().toLocaleDateString("en-ca");
+  return (
+    <div>
+      <Link href="/">
+        <a className="flex justify-end m-2 p-2 text-slate-700">
+          Return to homepage
+        </a>
+      </Link>
+      <p>Sale: {sale._id}</p>
+      <div className="mt-6 mx-auto w-fit flex flex-col justify-center bg-slate-100 shadow-lg p-4 rounded-lg">
+        <form onSubmit={handleSubmit}>
+          <h2 className="text-slate-900 text-lg mb-2 font-medium">Edit Sale</h2>
+          <label className="block m-1">
+            <span className="block text-sm font-medium text-slate-700">
+              Client name
+            </span>
+            <input
+              onChange={(e) => setClientName(e.target.value)}
+              value={clientName}
+              required
+              placeholder="E.g. Jane Doe"
+              type="text"
+              className="rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+          </label>
+          <label className="block m-1">
+            <span className="block text-sm font-medium text-slate-700">
+              Product name
+            </span>
+            <input
+              id="product"
+              onChange={(e) => setProducName(e.target.value)}
+              value={productName}
+              required
+              placeholder="E.g. Magic wand"
+              type="text"
+              className="rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+          </label>
+          <label className="block m-1">
+            <span className="block text-sm font-medium text-slate-700">
+              Price R$
+            </span>
+            <input
+              onChange={(e) => setPrice(e.target.value)}
+              value={price}
+              min="0"
+              required
+              step=".01"
+              placeholder="E.g. 249.90"
+              type="number"
+              className="rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+          </label>
+          <label className="block m-1">
+            <span className="block text-sm font-medium text-slate-700">
+              Date
+            </span>
+            <input
+              onChange={(e) => setDate(e.target.value)}
+              value={date}
+              required
+              type="date"
+              max={dateMax}
+              className="rounded-none relative block w-full px-3 py-2 border border-gray-300 invalid:text-grey-500 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+            />
+          </label>
+          <input
+            type="submit"
+            value="submit"
+            className="uppercase font-semibold text-xs flex ml-auto px-5 mt-4 p-2 rounded-lg hover:bg-slate-900 hover:text-slate-100 bg-slate-400 text-slate-900"
+          />
+        </form>
+      </div>
+    </div>
+  );
 };
 
-export default Sale;
+export default Edit;
 
 export async function getStaticProps(context) {
+  // get sale accessed
   const sale = await getSaleById(context.params.sid);
   return {
     props: {
@@ -29,8 +148,11 @@ export async function getStaticProps(context) {
     },
   };
 }
+// get paths allowed for this component
 export async function getStaticPaths() {
+  // getSalesIds returns an array only with the Ids of every registered sale
   const sales = await getSalesIds();
+  // transform data to return expected array of objects
   const paths = sales.map((sale) => ({
     params: { sid: sale._id },
   }));
--- pages/register.js
+++ pages/register.js
@@ -2,12 +2,8 @@ import { useRef, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useAuth } from "../context/AuthUserContext";
 import Link from "next/link";
-import {
-  getUsers,
-  createSale,
-  getAuthenticatedUser,
-  getMonthSales,
-} from "../lib/sanityData";
+import { createSale, getAuthenticatedUser } from "../lib/sanityData";
+import calculateCommission from "../lib/commission";
 
 export default function Register() {
   const router = useRouter();
@@ -28,40 +24,7 @@ export default function Register() {
     e.preventDefault();
     // get user object that matches seller logged in
     const seller = await getAuthenticatedUser(authUser.uid);
-    const monthSales = await getMonthSales();
-    // reduce array of sales to an object with "bonuses count" and total amount sold
-    const bonusCheck = monthSales.reduce(
-      (acc, cur) => {
-        acc.total += cur.price;
-        if (cur.price >= 1200) acc.case1++;
-        // count this bonus only if price is more than 800 but less than 1200
-        else if (cur.price >= 800) acc.case2++;
-        return acc;
-      },
-      { case1: 0, case2: 0, total: 0 }
-    );
-    let commission = 0;
-    const value = Number(price.current.value);
-    // calculate seller's commission amount
-    if (value <= 1600) {
-      commission = value * 0.04;
-      if (value < 1200) {
-        commission = value * 0.03;
-        if (value < 800) {
-          commission = value * 0.02;
-          if (value < 400) {
-            commission = value * 0.01;
-          }
-        }
-      }
-      // add 1% more if sells of the month exceed R$10000
-      if (bonusCheck.total > 10000) commission += value * 0.01;
-    }
-    // if it's the first sell above 1200 of the month, add extra R$ 100
-    if (!bonusCheck.case1 && value > 1200) commission += 100.0;
-    //if it's the first sell above 800 of the month, add extra R$ 50
-    if (!bonusCheck.case2 && value > 800) commission += 50.0;
-
+    const commission = await calculateCommission(price.current.value);
     const doc = {
       client: client.current.value,
       seller: {
@@ -71,7 +34,7 @@ export default function Register() {
       product: product.current.value,
       price: parseFloat(price.current.value),
       date: date.current.value,
-      commission: parseFloat(commission.toFixed(2)),
+      commission: commission,
       status: "pending", // new document so the sale is neither rejected or approved
     };
     createSale(doc)
@@ -79,7 +42,6 @@ export default function Register() {
       .catch((error) => {
         throw new Error(error.message);
       });
-
     client.current.value = "";
     product.current.value = "";
     price.current.value = "";
